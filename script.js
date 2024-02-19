97f17734abba720ed4342b4eb5de96f685b6ef02
fahimhossain26 <ismailhossainfahim64@gmail.com>
2024-02-19 09:35:00 +0600

    97% done js part

--- script.js
+++ script.js
@@ -7,24 +7,32 @@ const allBtn =document.getElementsByClassName('add-btn');
 console.log(allBtn);
 for(const btn of allBtn){
     btn.addEventListener('click',function(){
-        seatCount+=1;
+        
+         seatCount+=1;
+        if(seatCount>4){
+            btn.disabled = true;
+            ticketprice=ticketprice-550
+            alert('you can not buy more than 4 tickets')
+        }  
+        
+        
         document.getElementById('seat-count').innerText=seatCount;
         btn.classList.add('bg-lime-400');
         ticketprice+=550;
 
-      
+        
         const title =btn.querySelector('p').innerText;
         const titleContainer= document.getElementById('ticke_details');
          const p= document.createElement("p");
           p.innerText=title;
         titleContainer.appendChild(p);
 
-         document.getElementById('price').innerText=ticketprice;
+          document.getElementById('price').innerText=ticketprice;
 
         // seat remove 
         totalSeat-=1
        const removeSeat= document.getElementById('seatsRemove').innerText=totalSeat;
-
+        
     //    console.log(removeSeat);
         // console.log(totalPrice);
 
@@ -42,9 +50,23 @@ if(cuponElement==="NEW15"){
     discountElement.innerText=(ticketprice- disountedPrice);
 
     document.getElementById('grand-hidden').style.visibility="hidden";
+}
+
+//cupon 20
+
+else if(cuponElement==="Couple20"){
+    const discountElement= document.getElementById('grandTotalAmount');
+    const disountedPrice= ticketprice * 0.20;
+
+    discountElement.innerText=(ticketprice- disountedPrice);
+
+    document.getElementById('grand-hidden').style.visibility="hidden";
 
 
-}else{
+
+
+}
+else{
     alert('invalid cupon code');
 }
 //couple cupon 
